0d02968b6b8d2377095daed10dcc407b4f47bbcf
wydra76yan <yan@vydra.com>
2018-02-19 22:54:28 +0300

    Some features

--- ForAlert.js
+++ ForAlert.js
@@ -1,1 +1,13 @@
-alert( 'Привет, Мир!' );
+// var message;
+// message = 'Hello';
+// alert(message);
+// var que = prompt('How u do', 123);
+// alert('Du ' + que + ' Fo')
+ document.body.style.background = '#FF0040';
+// alert( location.href );
+function countCliks() {
+    for(var i=1; i<=3; i++) {
+        alert("Number of click " + i);
+    }
+}
+
