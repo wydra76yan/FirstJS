a99305859d6c291fddfa7e218ffc6561cc098352
wydra76yan <yan@vydra.com>
2018-02-20 00:34:52 +0300

    Play with colors

--- ForAlert.js
+++ ForAlert.js
@@ -1,13 +1,20 @@
-// var message;
 // message = 'Hello';
 // alert(message);
 // var que = prompt('How u do', 123);
 // alert('Du ' + que + ' Fo')
- document.body.style.background = '#FF0040';
+document.body.style.background = '#F5A9A9';
 // alert( location.href );
 function countCliks() {
     for(var i=1; i<=3; i++) {
         alert("Number of click " + i);
     }
 }
+function handl1() {
+    document.body.style.background='#58FA58'
+}
+function handl2() {
+    document.body.style.background='#58ACFA'
+}
+
+
 
