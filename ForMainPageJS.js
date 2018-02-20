672306f44febfbb2e7cd7824d7290f57a72eaee8
wydra76yan <yan@vydra.com>
2018-02-20 19:13:59 +0300

    Added simple image slider

---
+++ ForMainPageJS.js
@@ -0,0 +1,18 @@
+var i = 0;
+var images = [];
+var time = 3000;
+
+images[0] = "images/FirstSliderImage.jpg";
+images[1] = "images/SecondSliderImage.jpg";
+images[2] = "images/ThirdSliderImage.jpg";
+
+function changeImg(){
+    document.slide.src = images[i];
+    if(i < images.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout("changeImg()", time);
+}
+window.onload=changeImg;
