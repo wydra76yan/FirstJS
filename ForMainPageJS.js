62ff231ae8795c3111d610bda84a0b9947ff373f
wydra76yan <yan@vydra.com>
2018-02-22 00:09:49 +0300

    Added fade-in/fade-out animation

--- ForMainPageJS.js
+++ ForMainPageJS.js
@@ -1,19 +1,26 @@
 var i = 0;
 var images = [];
-var time = 3000;
+var time = 5000;
+var slide = document.getElementById('slide');
 
 images[0] = "images/FirstSliderImage.jpg";
 images[1] = "images/SecondSliderImage.jpg";
 images[2] = "images/ThirdSliderImage.jpg";
 
 function changeImg(){
-    document.slide.src = images[i];
+    //slide.classList.toggle('fadeIn');
+    document.getElementById('slide').src = images[i];
     if(i < images.length - 1){
         i++;
     } else {
         i = 0;
     }
+    setTimeout('slide.classList.toggle(\'fadeOut\')',2000);
     setTimeout("changeImg()", time);
 
+
 }
+slide.classList.toggle('fadeIn');
 window.onload=changeImg;
+
+
